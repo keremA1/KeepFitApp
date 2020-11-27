0e330dc2e114f01e3532c1193dd4eab5e37c4b48
Kerem <kalparslan82@gmail.com>
2020-11-27 13:19:46 +0000

    All classes updated

--- app/src/main/java/com/example/keepfitapp/Progress.java
+++ app/src/main/java/com/example/keepfitapp/Progress.java
@@ -2,13 +2,28 @@ package com.example.keepfitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Progress extends AppCompatActivity {
+
+    private Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ToHomePage = new Intent(Progress.this, HomeScreen.class);
+                startActivity(ToHomePage);
+            }
+
+        });
     }
 }
